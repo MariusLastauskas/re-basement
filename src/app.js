afc8084d284677655f75a8c49cb9be1eb05a0bb5
Marius Lastauskas <marius20122@gmail.com>
2020-10-13 08:25:43 +0300

    added example of authorization with github

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 import Vue from 'vue';
+import { get } from 'axios';
 
-Vue.component('hello-component', require('./components/hello').default);
+Vue.component('hello-component', {
+    template: `
+        <div>
+            <h2 class="text-center">Hello From Component</h2>
+            <button v-on:click="sign()">Sign in to git hub</button>
+        </div>
+    `,
+    methods: {
+        sign: () => {
+            get(
+                'https://github.com/login/oauth/authorize', 
+                {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Expose-Headers': 'ETag, Link, X-GitHub-OTP, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval'
+                    }
+                }
+            ).then(response => (this.info = response))
+        }
+    }
+});
 
 new Vue({
     el: '#app', 
     data: {
         message: 'Hello from Webpack'
-    }
+    },
 });
--- src/components/hello.vue
+++ src/components/hello.vue
@@ -1,8 +1,12 @@
-<template>
-    <h2 class="text-center">Hello From Component</h2>
-</template>
-<script>
-export default {
-    
-}
-</script>
+import { component } from 'vue';
+
+const hello = component('hello-component', {
+    template: `
+        <div>
+            <h2 class="text-center">Hello From Component</h2>
+            <button>Sign in to git hub</button>
+        </div>
+    `
+});
+
+export default hello;
